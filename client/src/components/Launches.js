d015fe19da8864e3b4c6c0d6ab286d98a63b259e
lajawanti <lajawanti.dhake@gmail.com>
2020-07-14 23:33:43 -0400

    Launches component

---
+++ client/src/components/Launches.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+const LAUNCHES_QUERY = gql `
+    query LaunchesQuery {
+        launches {
+            flight_number
+            mission_name
+            launch_date_local
+            launch_success
+        }
+    }
+`;
+
+export class Launches extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
